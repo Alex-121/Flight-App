43f22f895ed694fcea429ba6648ca04e6963ed3c
Aliyah Dumas <adumas4.student.gsu.edu>
2019-04-10 21:10:52 -0400

    class that will handle the communication to/from database(login/adduser)

---
+++ Flight_App/src/data/Logic.java
@@ -0,0 +1,40 @@
+package data;
+
+import database.Database;
+import userInterface.Alerts;
+
+public class Logic {
+	 Database d;
+	 Alerts a;
+	 Data data;
+	
+	
+	
+	
+	public void getData(Data example, String msg) throws Exception {
+		data = example;
+		
+		if(msg == "sign up")
+			addUser(example);
+		
+		else if(msg == "login")
+			login(example);
+	}
+	
+	private void login(Data example) throws Exception{
+		
+		d = new Database();
+		d.login(example);
+		d.closeConnection();
+		
+	}
+
+	public  void addUser(Data data) throws Exception{
+		
+		d = new Database();
+		d.addUser(data);
+		d.closeConnection();
+	}
+
+
+}
