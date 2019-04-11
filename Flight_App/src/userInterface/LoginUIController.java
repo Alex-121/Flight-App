31fe1c8998e781b6e57f50e760bab82fc0483c3b
Aliyah Dumas <adumas4.student.gsu.edu>
2019-04-10 21:11:17 -0400

    added functionality to login button

--- Flight_App/src/userInterface/LoginUIController.java
+++ Flight_App/src/userInterface/LoginUIController.java
@@ -1,18 +1,29 @@
 package userInterface;
 
 import java.io.IOException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
+import businessLogic.Person;
+import data.Data;
+import data.HandleExceptions;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class LoginUIController {
+	
+	@FXML
+	private TextField userName;
+	
+	@FXML
+	private PasswordField password;
 	
 	@FXML
 	private AnchorPane loginScreen; 
@@ -26,13 +37,27 @@ public class LoginUIController {
 	// Event Listener on ImageView.onMouseClicked
 	@FXML
 	public void close_application(MouseEvent event) {
-		if(ConfirmBox.display("System Exit", "Do you want to close the application?"))
 			System.exit(0);
 	}
 	// Event Listener on Button[#logIn].onMouseClicked
 	@FXML
 	public void loginClicked(MouseEvent event) {
 		
+		Person p = new Person();
+		p.setUserName(userName.getText());
+		p.setPass(password.getText());
+		Data d = new Data();
+		d.setPerson(p);
+		HandleExceptions h = new HandleExceptions();
+		try {
+			h.checkExceptions(d, "login");
+		} catch (SQLIntegrityConstraintViolationException e) {
+			// if there is a login error alert user(issue with username or password, try again)
+			if(e.getMessage() == "close") {
+				
+			}
+			e.printStackTrace();
+		}
 	}
 	// Event Listener on Button[#signUp].onMouseClicked
 	@FXML
@@ -43,6 +68,7 @@ public class LoginUIController {
 			Scene scene = new Scene(root);
 			Stage stage = new Stage();
 			stage.setScene(scene);
+			stage.setTitle("Sign up");
 			stage.show();
 		} catch (IOException e) {
 			
