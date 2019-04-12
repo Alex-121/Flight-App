5251c4787bc4f189d3d234c16b3410c4c5399ddc
Aliyah Dumas <adumas4.student.gsu.edu>
2019-04-12 02:21:11 -0400

    adjusted finally block

--- Flight_App/src/data/HandleExceptions.java
+++ Flight_App/src/data/HandleExceptions.java
@@ -2,12 +2,15 @@ package data;
 
 import java.sql.*;
 
+import database.Database;
+import userInterface.Alerts;
+
 public class HandleExceptions extends Logic{
 	
 
 
 	
-	public void checkExceptions(Data example, String msg) throws SQLIntegrityConstraintViolationException {
+	public void checkExceptions(Data example, String msg) throws Exception {
 		Logic t = new Logic();
 		
 		try {
@@ -23,13 +26,22 @@ public class HandleExceptions extends Logic{
 			//throw this back up so app can display alert about ssn duplication
 			throw new SQLIntegrityConstraintViolationException("SSN error");
 		} 
+		catch(customException ex) {
+			
+			if(ex.getMessage() == "User name not found. Please sign up")
+				Alerts.alert1(ex.getMessage());
+			throw ex ;
+		}
 		
 		catch(Exception ex) {
 			ex.printStackTrace();
 		}
 		finally {
-			//must wrap the close in a try catch
+			
+			
+			
 			try {
+				Database.closeConnection();
 				
 			} catch (Exception e) {
 				
